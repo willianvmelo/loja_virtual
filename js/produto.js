13f70292f607a1e90d37ea21996281426118d807
willianvmelo <willian.vieirademelo@gmail.com>
2021-04-08 17:43:59 -0300

    Adicionando Javascript

---
+++ js/produto.js
@@ -0,0 +1,10 @@
+var inputTamanho = document.querySelector('[name=tamanho]')
+
+var outputTamanho = document.querySelector('[name=valortamanho]')
+
+function mostraTamanho() {
+    outputTamanho.value = inputTamanho.value
+
+}
+
+inputTamanho.oninput = mostraTamanho()
